2a4cee2ba50e0aabdf57d280ba3740a01add7077
danwilkie <danwilkie777@gmail.com>
2017-10-04 13:37:06 +0100

    Rx Kata 1

--- src/main/java/learnrxjava/ObservableExercises.java
+++ src/main/java/learnrxjava/ObservableExercises.java
@@ -3,18 +3,22 @@ package learnrxjava;
 
 import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import learnrxjava.types.BoxArt;
 import learnrxjava.types.JSON;
+import learnrxjava.types.Movie;
 import learnrxjava.types.Movies;
+
+import static io.reactivex.Observable.just;
 
 public class ObservableExercises {
 
     /**
      * Return an Observable that emits a single value "Hello World!"
-     * 
+     *
      * @return "Hello World!"
      */
     public Observable<String> exerciseHello() {
-        return Observable.error(new RuntimeException("Not Implemented"));
+        return just("Hello World!");
     }
 
     /**
@@ -23,7 +27,7 @@ public class ObservableExercises {
      * @param "Hello Name!"
      */
     public Observable<String> exerciseMap(Observable<String> hello) {
-        return Observable.error(new RuntimeException("Not Implemented"));
+        return hello.map(it -> it + " Daniel!");
     }
 
     /**
@@ -34,63 +38,86 @@ public class ObservableExercises {
      * 6-Even
      */
     public Observable<String> exerciseFilterMap(Observable<Integer> nums) {
-        return Observable.error(new RuntimeException("Not Implemented"));
+        return nums
+                .filter(it -> it % 2 == 0)
+                .map(it -> it + "-Even");
     }
 
     /**
      * Flatten out all video in the stream of Movies into a stream of videoIDs
-     * 
+     *
      * @param movies
      * @return Observable of Integers of Movies.videos.id
      */
     public Observable<Integer> exerciseConcatMap(Observable<Movies> movies) {
-        return Observable.error(new RuntimeException("Not Implemented"));
+        return movies
+                .concatMap(it -> it.videos)
+                .map(it -> it.id);
     }
 
     /**
      * Flatten out all video in the stream of Movies into a stream of videoIDs
-     * 
+     * <p>
      * Use flatMap this time instead of concatMap. In Observable streams
      * it is almost always flatMap that is wanted, not concatMap as flatMap
      * uses merge instead of concat and allows multiple concurrent streams
      * whereas concat only does one at a time.
-     * 
+     * <p>
      * We'll see more about this later when we add concurrency.
-     * 
+     *
      * @param movies
      * @return Observable of Integers of Movies.videos.id
      */
     public Observable<Integer> exerciseFlatMap(Observable<Movies> movies) {
-        return Observable.error(new RuntimeException("Not Implemented"));
+        return movies
+                .flatMap(it -> it.videos)
+                .map(it -> it.id);
     }
 
     /**
      * Retrieve the largest number.
-     * 
+     * <p>
      * Use reduce to select the maximum value in a list of numbers.
      */
     public Maybe<Integer> exerciseReduce(Observable<Integer> nums) {
-        return Maybe.error(new RuntimeException("Not Implemented"));
+        return nums.reduce(Math::max);
     }
 
     /**
      * Retrieve the id, title, and <b>smallest</b> box art url for every video.
-     * 
+     * <p>
      * Now let's try combining reduce() with our other functions to build more complex queries.
-     * 
+     * <p>
      * This is a variation of the problem we solved earlier, where we retrieved the url of the boxart with a
      * width of 150px. This time we'll use reduce() instead of filter() to retrieve the _smallest_ box art in
      * the boxarts list.
-     * 
+     * <p>
      * See Exercise 19 of ComposableListExercises
      */
     public Observable<JSON> exerciseMovie(Observable<Movies> movies) {
-        return Observable.error(new RuntimeException("Not Implemented"));
+        return movies.
+                flatMap(it -> it.videos)
+                .map(movie -> json(
+                        "id", movie.id,
+                        "title", movie.title,
+                        "smallestBoxArt", smallestBoxArtUrl(movie)));
+
+    }
+
+    private String smallestBoxArtUrl(Movie movie) {
+        return movie.boxarts
+                .reduce((boxArt1, boxArt2) -> area(boxArt1) <= area(boxArt2) ? boxArt1 : boxArt2)
+                .map(boxArt -> boxArt.url)
+                .blockingGet();
+    }
+
+    private int area(BoxArt boxArt) {
+        return boxArt.width * boxArt.height;
     }
 
     /**
      * Combine 2 streams into pairs using zip.
-     * 
+     * <p>
      * a -> "one", "two", "red", "blue"
      * b -> "fish", "fish", "fish", "fish"
      * output -> "one fish", "two fish", "red fish", "blue fish"
