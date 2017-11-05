d36b0f1f4d76d460f8e1f48b7db21d28d1939c3b
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2017-11-05 22:42:52 +0000

    Update exercise info

--- src/main/java/novoda/BasicExercises.kt
+++ src/main/java/novoda/BasicExercises.kt
@@ -9,7 +9,6 @@ class BasicExercises {
     private val INTEGERS = Arrays.asList(0, 1, 2, 3, 4, 5, 6)
 
     /**
-    - Multiply all INTEGERS elements by 2
     - Repeat 3 times every even element and emit once every odd element
     - Send elements until you encounter an odd element then fail, restart and multiply by 2 the entire sequence
     - Prepend the string "Integer : " in front of every element
