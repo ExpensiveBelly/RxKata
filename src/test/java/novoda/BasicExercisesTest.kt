6e799315a606641d97fc550398abe6ae8423956a
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2017-11-09 22:32:21 +0000

    Timer exercise

--- src/test/java/novoda/BasicExercisesTest.kt
+++ src/test/java/novoda/BasicExercisesTest.kt
@@ -1,15 +1,20 @@
 package novoda
 
+import io.reactivex.schedulers.TestScheduler
 import org.junit.Before
 import org.junit.Test
+import java.util.concurrent.TimeUnit
+
 
 class BasicExercisesTest {
 
     private lateinit var exercises: BasicExercises
+    private lateinit var testScheduler: TestScheduler
 
     @Before
     fun setUp() {
         exercises = BasicExercises()
+        testScheduler = TestScheduler()
     }
 
     @Test
@@ -34,5 +39,23 @@ class BasicExercisesTest {
         exercises.infiniteExercise()
                 .test()
                 .assertResult("0:This is the first sentence 1:I want those to be enumerated 2:How would you ask? 3:That is yours to find out!")
+    }
+
+
+    @Test
+    fun timer() {
+        val test = exercises.timer(testScheduler).test()
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+        test.assertValueAt(0, 1L)
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+        test.assertValueAt(1, 2L)
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+        test.assertValueAt(2, 3L)
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+        test.assertValueAt(3, 4L)
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+        test.assertValueAt(4, 5L)
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+        test.assertValueAt(5, 6L)
     }
 }
