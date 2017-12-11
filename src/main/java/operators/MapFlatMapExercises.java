17ce06b27aa8c740552219b9e65c3440efd1b703
Andy Roberts <andrew.roberts86@hotmail.co.uk>
2017-12-11 13:26:48 +0000

    More tests

--- src/main/java/operators/MapFlatMapExercises.java
+++ src/main/java/operators/MapFlatMapExercises.java
@@ -5,6 +5,7 @@ import org.javatuples.Pair;
 import org.javatuples.Triplet;
 
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -22,106 +23,108 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class MapFlatMapExercises {
 
-	/**
-	 * Given a stream t[ [1,2,3],[4,5,6],[7,8,9] ] which has "two levels", flatten it in a "one level" structure :
-	 * [ 1,2,3,4,5,6,7,8,9 ]
-	 */
-	public Observable<Integer> exerciseFlatten() {
-		Observable<Triplet<Integer, Integer, Integer>> pairObservable = Observable
-				.just(new Triplet<>(1, 2, 3),
-						new Triplet<>(4, 5, 6),
-						new Triplet<>(7, 8, 9));
+    /**
+     * Given a stream t[ [1,2,3],[4,5,6],[7,8,9] ] which has "two levels", flatten it in a "one level" structure :
+     * [ 1,2,3,4,5,6,7,8,9 ]
+     */
+    public Observable<Integer> exerciseFlatten() {
+        Observable<Triplet<Integer, Integer, Integer>> pairObservable = Observable
+                .just(new Triplet<>(1, 2, 3),
+                        new Triplet<>(4, 5, 6),
+                        new Triplet<>(7, 8, 9));
 
-		return null;
-	}
+        return pairObservable.flatMap(triplet -> Observable.just(triplet.getValue0(), triplet.getValue1(), triplet.getValue2()));
+    }
 
-	/**
-	 * You have a booking that has an optional user. And the optional user has an optional email.
-	 * Write a method getEmail() that gives you the email option of a booking
-	 *
-	 * @return
-	 */
+    /**
+     * You have a booking that has an optional user. And the optional user has an optional email.
+     * Write a method getEmail() that gives you the email option of a booking
+     *
+     * @return
+     */
 
-	public Observable<String> exerciseEmail() {
-		Booking booking = new Booking(new Booking.User("myemail@gmail.com"));
+    public Observable<String> exerciseEmail() {
+        Booking booking = new Booking(new Booking.User("myemail@gmail.com"));
 
-		return null;
-	}
+        return null;
+    }
 
-	static class Booking {
-		private final Optional<User> user;
+    static class Booking {
+        private final Optional<User> user;
 
-		public Booking(User user) {
-			this.user = Optional.ofNullable(user);
-		}
+        public Booking(User user) {
+            this.user = Optional.ofNullable(user);
+        }
 
-		Optional<User> getUser() {
-			return user;
-		}
+        Optional<User> getUser() {
+            return user;
+        }
 
-		private static class User {
-			Optional<String> getEmail() {
-				return email;
-			}
+        private static class User {
+            Optional<String> getEmail() {
+                return email;
+            }
 
-			private final Optional<String> email;
+            private final Optional<String> email;
 
-			public User(String email) {
-				this.email = Optional.ofNullable(email);
-			}
-		}
-	}
+            public User(String email) {
+                this.email = Optional.ofNullable(email);
+            }
+        }
+    }
 
-	/**
-	 * Return an Observable that shows the records as specified in the test using `loadRecordsFor` method as the source of
-	 * the data
-	 */
+    /**
+     * Return an Observable that shows the records as specified in the test using `loadRecordsFor` method as the source of
+     * the data
+     */
 
-	public Observable<String> loadRecordsExercise() {
-		/*return*/ Observable
-				.just(DayOfWeek.SUNDAY, DayOfWeek.MONDAY);
-				/* loadRecordsFor() */
+    public Observable<String> loadRecordsExercise() {
+        /*return*/
+        Observable
+                .just(DayOfWeek.SUNDAY, DayOfWeek.MONDAY);
+                /* loadRecordsFor() */
 
-		return null;
-	}
+        return null;
+    }
 
-	Observable<String> loadRecordsFor(DayOfWeek dow) {
-		switch (dow) {
-			case SUNDAY:
-				return Observable
-						.interval(90, MILLISECONDS)
-						.take(5)
-						.map(i -> "Sun-" + i);
-			case MONDAY:
-				return Observable
-						.interval(65, MILLISECONDS)
-						.take(5)
-						.map(i -> "Mon-" + i);
-		}
-		return null;
-	}
+    Observable<String> loadRecordsFor(DayOfWeek dow) {
+        switch (dow) {
+            case SUNDAY:
+                return Observable
+                        .interval(90, MILLISECONDS)
+                        .take(5)
+                        .map(i -> "Sun-" + i);
+            case MONDAY:
+                return Observable
+                        .interval(65, MILLISECONDS)
+                        .take(5)
+                        .map(i -> "Mon-" + i);
+        }
+        return null;
+    }
 
-	/**
-	 * TODO: Exercise is finished, just make the test pass
-	 * Take an arbitrary text in String and split it to words, removing punctuation using a
-	 * regular expression. Now, for each word we calculate how much it takes to say that word,
-	 * simply by multiplying the word length by millisPerChar (Check 'Speak' class)
-	 * Then, we would like to spread words over time, so that each word appears in the resulting stream after the delay calculated
-	 * @param quote
-	 * @param millisPerChar
-	 * @return
-	 */
+    /**
+     * TODO: Exercise is finished, just make the test pass
+     * Take an arbitrary text in String and split it to words, removing punctuation using a
+     * regular expression. Now, for each word we calculate how much it takes to say that word,
+     * simply by multiplying the word length by millisPerChar (Check 'Speak' class)
+     * Then, we would like to spread words over time, so that each word appears in the resulting stream after the delay calculated
+     *
+     * @param quote
+     * @param millisPerChar
+     * @return
+     */
 
-	Observable<String> speak(String quote, long millisPerChar) {
-		String[] tokens = quote.replaceAll("[:,]", "").split(" ");
-		Observable<String> words = Observable.fromArray(tokens);
-		Observable<Long> absoluteDelay = words
-				.map(String::length)
-				.map(len -> len * millisPerChar)
-				.scan((total, current) -> total + current);
-		return words
-				.zipWith(absoluteDelay.startWith(0L), Pair::new)
-				.flatMap(pair -> Observable.just(pair.getValue0())
-						.delay(pair.getValue1(), MILLISECONDS));
-	}
+    Observable<String> speak(String quote, long millisPerChar) {
+        String[] tokens = quote.replaceAll("[:,]", "").split(" ");
+        Observable<String> words = Observable.fromArray(tokens);
+        Observable<Long> absoluteDelay = words
+                .map(String::length)
+                .map(len -> len * millisPerChar)
+                .scan((total, current) -> total + current);
+        return words
+                .zipWith(absoluteDelay.startWith(0L), Pair::new)
+                .flatMap(pair -> Observable.just(pair.getValue0())
+                        .delay(pair.getValue1(), MILLISECONDS));
+    }
 }
