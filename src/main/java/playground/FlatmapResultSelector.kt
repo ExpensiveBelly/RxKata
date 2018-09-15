f0f1d738c96813f33aa5b1ca6ecc919c8a19b106
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2018-09-15 16:09:23 +0100

    Extract Result to its own class

--- src/main/java/playground/FlatmapResultSelector.kt
+++ src/main/java/playground/FlatmapResultSelector.kt
@@ -29,8 +29,3 @@ class FlatmapResultSelector {
     }
 
 }
-
-sealed class Result<out T, out R> {
-    data class Success<out T>(val resp: T) : Result<T, Nothing>()
-    data class ErrorResult<out R>(val error: R) : Result<Nothing, R>()
-}
---
+++ src/main/java/playground/Result.kt
@@ -0,0 +1,6 @@
+package playground
+
+sealed class Result<out T, out R> {
+    data class Success<out T>(val resp: T) : Result<T, Nothing>()
+    data class ErrorResult<out R>(val error: R) : Result<Nothing, R>()
+}
