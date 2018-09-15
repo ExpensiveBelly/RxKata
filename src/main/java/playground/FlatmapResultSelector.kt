313f37ddf79fcac9cdd6f6a5f6499fba5dbf8ec4
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2018-09-15 16:06:55 +0100

    Flatmap result selector with test

---
+++ src/main/java/playground/FlatmapResultSelector.kt
@@ -0,0 +1,36 @@
+package playground
+
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * https://medium.com/@douglas.iacovelli/rxjava-kotlin-keep-the-original-value-in-a-flatmap-bbd6a6974a99
+ */
+
+class FlatmapResultSelector {
+
+    private val updateUserName: UpdateUserName = UpdateUserName()
+
+    class UpdateUserName {
+        fun execute(name: String): Observable<Result<String, Throwable>> =
+                Observable.just(Result.Success("Expensive"))
+    }
+
+    fun `flatmap with result selector`(): Observable<Pair<String, Result<String, Throwable>>> {
+        return Observable.just("Will")
+                .observeOn(Schedulers.io())
+                .flatMap({ name ->
+                    updateUserName.execute(name) //returns another Observable<Result> after calling the API
+                }, {
+                    //this is executed after resolving the last observable emitted, so the result is ready.
+                    name: String, apiResult: Result<String, Throwable> ->
+                    Pair(name, apiResult)
+                })
+    }
+
+}
+
+sealed class Result<out T, out R> {
+    data class Success<out T>(val resp: T) : Result<T, Nothing>()
+    data class ErrorResult<out R>(val error: R) : Result<Nothing, R>()
+}
