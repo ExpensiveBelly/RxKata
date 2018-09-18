56fabb39442a354eb06b1fd5a48204816b90b336
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2018-09-18 21:27:55 +0100

    Remove warnings from the test

--- src/test/java/playground/BasketPresenterTest.kt
+++ src/test/java/playground/BasketPresenterTest.kt
@@ -16,35 +16,35 @@ class BasketPresenterTest {
     @get:Rule
     val schedulerRule = TrampolineSchedulerRule()
 
-    val secretKey = "1234"
+    private val secretKey = "1234"
 
-    val sessionApiSubject = SingleSubject.create<SessionTO>()
-    val sessionApi = mock<SessionApi> {
+    private val sessionApiSubject = SingleSubject.create<SessionTO>()
+    private val sessionApi = mock<SessionApi> {
         on { login(any(), any()) } doReturn sessionApiSubject
     }
 
-    val productId1 = "productId1"
-    val productId2 = "productId2"
-    val productId3 = "productId3"
-    val basketId = "1"
-    val getBasketSubject = SingleSubject.create<List<BasketTO>>()
-    val basketApi = mock<BasketApi> {
+    private val productId1 = "productId1"
+    private val productId2 = "productId2"
+    private val productId3 = "productId3"
+    private val basketId = "1"
+    private val getBasketSubject = SingleSubject.create<List<BasketTO>>()
+    private val basketApi = mock<BasketApi> {
         on { getBaskets(secretKey) } doReturn getBasketSubject
     }
 
-    val productTO3 = ProductTO(productId1, "Lettuce", "VEGETABLES")
-    val productTO1 = ProductTO(productId2, "Diced Chicken", "MEAT")
-    val productTO2 = ProductTO(productId3, "Banana", "FRUIT")
-    val product1ApiSubject = SingleSubject.create<ProductTO>()
-    val product2ApiSubject = SingleSubject.create<ProductTO>()
-    val product3ApiSubject = SingleSubject.create<ProductTO>()
-    val productsApi = mock<ProductsApi> {
+    private val productTO3 = ProductTO(productId1, "Lettuce", "VEGETABLES")
+    private val productTO1 = ProductTO(productId2, "Diced Chicken", "MEAT")
+    private val productTO2 = ProductTO(productId3, "Banana", "FRUIT")
+    private val product1ApiSubject = SingleSubject.create<ProductTO>()
+    private val product2ApiSubject = SingleSubject.create<ProductTO>()
+    private val product3ApiSubject = SingleSubject.create<ProductTO>()
+    private val productsApi = mock<ProductsApi> {
         on { productsSingle(secretKey, productId1) } doReturn product1ApiSubject
         on { productsSingle(secretKey, productId2) } doReturn product2ApiSubject
         on { productsSingle(secretKey, productId3) } doReturn product3ApiSubject
     }
 
-    val view = mock<ShoppingBasketView> {}
+    private val view = mock<ShoppingBasketView> {}
 
     private val presenter: BasketPresenter = BasketPresenter(
             BasketRepository(
