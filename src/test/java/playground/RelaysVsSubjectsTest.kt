125744f85150c431f91d1c161f4fb1ff1e3ac25f
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2018-09-30 20:14:16 +0100

    Playing with Subjects and Relays

--- src/test/java/playground/RelaysVsSubjectsTest.kt
+++ src/test/java/playground/RelaysVsSubjectsTest.kt
@@ -1,16 +1,21 @@
 package playground
 
+import com.jakewharton.rxrelay2.PublishRelay
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
+import org.amshove.kluent.shouldEqual
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.Test
 import java.io.IOException
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class RelaysVsSubjectsTest {
 
     private val loadMoreSubject: PublishSubject<Unit> = PublishSubject.create()
+    private val loadMoreRelay: PublishRelay<Unit> = PublishRelay.create()
 
     @Test
     fun subjects_do_not_retry_after_error() {
@@ -27,11 +32,41 @@ class RelaysVsSubjectsTest {
                 }
                 .test()
 
-        clickLoadMore()
+        loadMoreSubject.onNext(Unit)
 
         latch.await()
 
-        observable.assertError(IOException::class.java).also { clickLoadMore() }
+        observable.assertError(IOException::class.java).also { loadMoreSubject.onNext(Unit) }
+        assertThat(actuallyReceived.get()).isEqualTo(1)
+    }
+
+    /**
+     * “Relays do not help us if we get an error while consuming the events from it and mapping into something else
+     * i.e. downstream. In fact, a Relay would also let the stream crash just like a Subject in this case.
+     * On the other hand, Relays are beneficial because they do not accept any terminal event from the producer,
+     * i.e from the top.”
+     */
+
+    @Test
+    fun relays_do_not_retry_after_error() {
+        val storyRepository = StoryRepository()
+        val latch = CountDownLatch(1)
+        val actuallyReceived = AtomicInteger()
+
+        val observable = loadMoreRelay
+                .flatMap {
+                    storyRepository.fetchNextStoriesThrowsIOException()
+                }.doOnEach {
+                    actuallyReceived.incrementAndGet()
+                    latch.countDown()
+                }
+                .test()
+
+        loadMoreRelay.accept(Unit)
+
+        latch.await()
+
+        observable.assertError(IOException::class.java).also { loadMoreRelay.accept(Unit) }
         assertThat(actuallyReceived.get()).isEqualTo(1)
     }
 
@@ -51,15 +86,52 @@ class RelaysVsSubjectsTest {
                 }
                 .test()
 
-        clickLoadMore()
+        loadMoreSubject.onNext(Unit)
 
         latch.await()
 
-        observable.assertNoErrors().also { clickLoadMore() }
+        observable.assertNoErrors().also { loadMoreSubject.onNext(Unit) }
         assertThat(actuallyReceived.get()).isEqualTo(2)
     }
 
-    private fun clickLoadMore() = loadMoreSubject.onNext(Unit)
+    @Test
+    fun subjects_consume_complete_or_error() {
+        val result: MutableList<Int> = mutableListOf()
+        val completingObservable = Observable.just(1)
+                .doOnNext { result.add(it) }
+
+        val subject = PublishSubject.create<Int>()
+        completingObservable.subscribe(subject)
+
+        //subject has received the onComplete() event from completingObservable and it has terminated
+        assertTrue(subject.test().isTerminated)
+
+        //Subsequent emissions are ignored
+        val completingObservable2 = Observable.just(2)
+                .doOnNext { result.add(it) }
+        completingObservable2.subscribe(subject)
+
+        result shouldEqual listOf(1)
+    }
+
+    @Test
+    fun relays_do_not_consume_complete_or_error() {
+        val result: MutableList<Int> = mutableListOf()
+        val completingObservable = Observable.just(1)
+                .doOnNext { result.add(it) }
+
+        val relay = PublishRelay.create<Int>()
+        completingObservable.subscribe(relay)
+
+        //Relay does not consume the onComplete() event from completingObservable
+        assertFalse(relay.test().isTerminated)
+
+        val completingObservable2 = Observable.just(2)
+                .doOnNext { result.add(it) }
+        completingObservable2.subscribe(relay)
+
+        result shouldEqual listOf(1, 2)
+    }
 
     class StoryRepository {
 
