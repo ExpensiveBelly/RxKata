283b99ef6a395957f856e345b9e36dbbd90fddfe
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2018-11-25 10:46:32 +0000

    Alternative solution to loadFavourites

--- src/main/java/hannesdorfmann/ex3/RepositoryEx3.kt
+++ src/main/java/hannesdorfmann/ex3/RepositoryEx3.kt
@@ -2,6 +2,8 @@ package hannesdorfmann.ex3
 
 import hannesdorfmann.types.PersonWithAddress
 import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Schedulers
 
 internal class RepositoryEx3(private val backendEx3: BackendEx3, private val contactsDatabase: ContactsDatabase) {
 
@@ -12,13 +14,21 @@ internal class RepositoryEx3(private val backendEx3: BackendEx3, private val con
          * Provide an observable that only emits a list of PersonWithAddress if they are marked as favorite ones.
          */
 
-        return contactsDatabase.favoriteContacts()
-                .flatMap { integers ->
-                    backendEx3.loadAllPersons()
-                            .flatMap<PersonWithAddress>({ Observable.fromIterable(it) })
-                            .filter { (person) -> integers.contains(person.id) }
-                            .toList()
-                            .toObservable()
-                }
+//        Solution 1
+//        return contactsDatabase.favoriteContacts()
+//                .flatMap { integers ->
+//                    backendEx3.loadAllPersons()
+//                            .flatMap<PersonWithAddress>({ Observable.fromIterable(it) })
+//                            .filter { (person) -> integers.contains(person.id) }
+//                            .toList()
+//                            .toObservable()
+//                }
+
+        // Solution 2
+        return Observable.combineLatest(contactsDatabase.favoriteContacts().subscribeOn(Schedulers.io()),
+                backendEx3.loadAllPersons().subscribeOn(Schedulers.io()),
+                BiFunction { favouriteContactsIds, personsWithAddress ->
+                    personsWithAddress.filter { it.person.id in favouriteContactsIds }
+                })
     }
 }
