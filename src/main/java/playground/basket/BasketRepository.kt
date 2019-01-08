febc6d7db500fa1b7a0d776b23394ffa7f61c6b8
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-01-08 20:17:10 +0000

    Basket exercise alternative approach using maxConcurrency
    Use flatmap overloaded version to limit the amount of threads
    spawned in parallel

--- src/main/java/playground/basket/BasketRepository.kt
+++ src/main/java/playground/basket/BasketRepository.kt
@@ -1,7 +1,10 @@
 package playground.basket
 
+import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.SingleTransformer
 import io.reactivex.schedulers.Schedulers
+
 
 class BasketRepository(private val sessionRepository: SessionRepository,
                        private val basketApi: BasketApi,
@@ -14,22 +17,53 @@ class BasketRepository(private val sessionRepository: SessionRepository,
                         .subscribeOn(Schedulers.io())
                         .observeOn(Schedulers.computation())
                         .doOnError { reportIfSessionInvalid() }
-                        .flatMap { basketTOs ->
-                            Single.zip(basketTOs.map { basketTO ->
-                                Single.zip(basketTO.productIds.map { productId ->
+                        .compose(fetchGetProductsNoConcurrencyLimit(sessionKey))
+            }
+            .replay(1).refCount()
+
+    private fun reportIfSessionInvalid(): (Throwable) -> Unit =
+            { if (it is ConnectionError && it.errorType == ConnectionErrorType.SESSION_INVALID) sessionRepository.reportSessionInvalid() }
+
+    /**
+     * Limits concurrency to 10 threads in parallel
+     */
+
+    private fun fetchGetProductsMaxConcurrency(sessionKey: String, maxConcurrency: Int = 10): SingleTransformer<List<BasketTO>, List<BasketItem>> {
+        return SingleTransformer { basketToList ->
+            basketToList.flattenAsObservable { it }
+                    .flatMapSingle { basketTO ->
+                        Observable.fromIterable(basketTO.productIds)
+                                .flatMap({ productId ->
                                     productsApi.getProducts(sessionKey, productId)
                                             .subscribeOn(Schedulers.io())
                                             .observeOn(Schedulers.computation())
                                             .doOnError { reportIfSessionInvalid() }
                                             .map { it.toProduct() }
-                                }) { list -> list.map { it as Product } }.map { BasketItem(basketTO.id, basketTO.name, it) }
-                            }) { list -> list.map { it as BasketItem } }
-                        }
-            }.replay(1).refCount()
+                                            .toObservable()
+                                }, maxConcurrency)
+                                .toList()
+                                .map { BasketItem(basketTO.id, basketTO.name, it) }
+                    }.toList()
+        }
+    }
 
-    private fun reportIfSessionInvalid(): (Throwable) -> Unit =
-            { if (it is ConnectionError && it.errorType == ConnectionErrorType.SESSION_INVALID) sessionRepository.reportSessionInvalid() }
+    private fun fetchGetProductsNoConcurrencyLimit(sessionKey: String): SingleTransformer<List<BasketTO>, List<BasketItem>> {
+        return SingleTransformer { basketToList ->
+            basketToList.flatMap { basketTOs ->
+                Single.zip(basketTOs.map { basketTO ->
+                    Single.zip(basketTO.productIds.map { productId ->
+                        productsApi.getProducts(sessionKey, productId)
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(Schedulers.computation())
+                                .doOnError { reportIfSessionInvalid() }
+                                .map { it.toProduct() }
+                    }) { list -> list.map { it as Product } }.map { BasketItem(basketTO.id, basketTO.name, it) }
+                }) { list -> list.map { it as BasketItem } }
+            }
+        }
+    }
 }
+
 
 fun ProductTO.toProduct() = Product(this.id, this.name,
         when (type) {
