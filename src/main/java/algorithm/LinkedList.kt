dc48fd2215d305026e40a775a1c4ef7323366dbd
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-01-20 18:31:46 +0000

    Code Fights: LinkedList second exercise

--- src/main/java/algorithm/LinkedList.kt
+++ src/main/java/algorithm/LinkedList.kt
@@ -2,20 +2,63 @@ package algorithm
 
 fun main() {
     //3, 1, 2, 3, 4, 5
-    val first = ListNode(3)
-    val second = ListNode(1)
-    val third = ListNode(2)
-    val fourth = ListNode(3)
-    val fifth = ListNode(4)
-    val sixth = ListNode(5)
+    val first = ListNode(1)
+    val second = ListNode(2)
+    val third = ListNode(3)
+    val fourth = ListNode(2)
+    val fifth = ListNode(1)
+//    val sixth = ListNode(1)
 
     first.next = second
     second.next = third
     third.next = fourth
     fourth.next = fifth
-    fifth.next = sixth
+//    fifth.next = sixth
 
-    println(removeKFromList(first, 3))
+//    println(removeKFromList(first, 3))
+    println(isListPalindrome(first))
+}
+
+/**
+ * Given a singly linked list of integers, determine whether or not it's a palindrome.
+ */
+
+fun isListPalindrome(l: ListNode<Int>?): Boolean {
+    if (l?.next == null) return true
+    //Possible Solution: move pointer to the middle of the list, reverse the other half, and start comparing from there.
+    //010, 0110, 0111110
+
+    //How many nodes in the list?
+    var tail = l
+    var count = 1
+    while (tail?.next != null) {
+        tail = tail.next
+        count++
+    }
+
+    val halfCount = count / 2
+
+    //Reverse first half of the list
+    var head = l
+    var prev: ListNode<Int>? = null
+    var current = head
+    var next = head?.next
+    repeat(halfCount) {
+        current?.next = prev
+        prev = current
+        current = next
+        next = next?.next
+    }
+
+    head = prev
+    next = if (count % 2 == 0) current else next
+    repeat(halfCount) {
+        if (head?.value != next?.value) return false
+        head = head?.next
+        next = next?.next
+    }
+
+    return true
 }
 
 data class ListNode<T>(var value: T) {
