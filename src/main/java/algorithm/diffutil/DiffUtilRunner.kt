7a560fccf918809e5e27d60c042c01bb17b7b796
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-09-01 20:51:56 +0100

    Include LICENSE and make the exercises clearer

--- src/main/java/algorithm/diffutil/DiffUtilRunner.kt
+++ src/main/java/algorithm/diffutil/DiffUtilRunner.kt
@@ -4,20 +4,20 @@ import algorithm.diffutil.callback.DefaultDiffUtilCallback
 import algorithm.diffutil.callback.ExpandableListDiffCallback
 
 fun main() {
-//    flattenedLists(listOf("Hello"), listOf("Hello", "World"))
+    flattenedLists(listOf("Hello"), listOf("Hello", "World"))
 
-    nestedLists(listOf(Nested("Summary 1", emptyList())), listOf(Nested("Summary 2", emptyList())))
+    nestedLists(listOf(Nested("Summary 1", listOf("Hello"))), listOf(Nested("Summary 2", listOf("Hello", "World"))))
 }
 
 data class Nested(val summary: String, val items: List<String>)
 
 fun nestedLists(oldList: List<Nested>, newList: List<Nested>) {
-    println("Nested lists")
+    println("** Nested lists **")
     calculateDiffUtil(oldList, newList, ExpandableListDiffCallback(oldList, newList, oldList.map { it.items.size }, newList.map { it.items.size }))
 }
 
 private fun flattenedLists(oldList: List<String>, newList: List<String>) {
-    println("Flattened lists")
+    println("** Flattened lists **")
     calculateDiffUtil(oldList, newList, DefaultDiffUtilCallback(oldList, newList, EqualsDiffCallback()))
 }
 
