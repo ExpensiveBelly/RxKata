2e4e449efc814253c762c8cb1a6620d1d3393712
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-09-05 21:48:28 +0100

    Both emitter.seCancelable and setDisposable dispose

---
+++ src/test/java/playground/curiosities/CancelableVsDisposableTest.kt
@@ -0,0 +1,48 @@
+package playground.curiosities
+
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposables
+import io.reactivex.schedulers.TestScheduler
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+import kotlin.test.assertTrue
+
+
+class CancelableVsDisposableTest {
+
+    @Test
+    fun cancelable_should_dispose_the_stream() {
+        val testScheduler = TestScheduler()
+        var innerDisposable = Disposables.disposed()
+        val testObserver = Observable.create<Int> { emitter ->
+            innerDisposable = Observable.interval(0, 100, TimeUnit.MILLISECONDS, testScheduler)
+                    .doOnNext { emitter.onNext(it.toInt()) }.subscribe()
+            emitter.setCancellable { innerDisposable.dispose() }
+        }.test()
+
+        testObserver.assertSubscribed()
+        testScheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS)
+        testObserver.assertValues(0, 1)
+        testObserver.dispose()
+
+        assertTrue { innerDisposable.isDisposed }
+    }
+
+    @Test
+    fun disposable_should_dispose_the_stream() {
+        val testScheduler = TestScheduler()
+        var innerDisposable = Disposables.disposed()
+        val testObserver = Observable.create<Int> { emitter ->
+            innerDisposable = Observable.interval(0, 100, TimeUnit.MILLISECONDS, testScheduler)
+                    .doOnNext { emitter.onNext(it.toInt()) }.subscribe()
+            emitter.setDisposable(Disposables.fromAction { innerDisposable.dispose() })
+        }.test()
+
+        testObserver.assertSubscribed()
+        testScheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS)
+        testObserver.assertValues(0, 1)
+        testObserver.dispose()
+
+        assertTrue { innerDisposable.isDisposed }
+    }
+}
