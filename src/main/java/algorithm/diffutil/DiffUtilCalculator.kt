b67925648bcefe7092edd9a606b007307978f510
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-09-24 21:28:10 +0100

    Remove DiffUtil because it's been moved to its own repo

--- src/main/java/algorithm/diffutil/DiffUtilCalculator.kt
+++
@@ -1,7 +0,0 @@
-package algorithm.diffutil
-
-
-fun calculateDiffUtilAndDispatch(diffCallback: DiffUtil.Callback, listUpdateCallback: ListUpdateCallback) = calculateDiffResult(diffCallback).dispatchUpdatesTo(listUpdateCallback)
-
-fun calculateDiffResult(diffCallback: DiffUtil.Callback) = DiffUtil.calculateDiff(diffCallback)
-
--- src/main/java/algorithm/diffutil/EqualsDiffCallback.kt
+++
@@ -1,7 +0,0 @@
-package algorithm.diffutil
-
-class EqualsDiffCallback<T> : DiffUtil.ItemCallback<T>() {
-    override fun areItemsTheSame(item1: T, item2: T) = areContentsTheSame(item1, item2)
-
-    override fun areContentsTheSame(item1: T, item2: T) = item1 == item2
-}
