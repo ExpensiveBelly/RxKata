7db4feb6c4958a7d2527d084c0b40b8b3b109d91
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-09-29 16:12:20 +0100

    Algorithms not related to Rx have been moved to its own repo

--- src/main/java/algorithm/RecursionExercises.kt
+++
@@ -1,9 +0,0 @@
-package algorithm
-
-
-fun toBinary(number: Int): String =
-        when (number) {
-            0 -> "0"
-            1 -> "1"
-            else -> "${toBinary(number / 2)}${number % 2}"
-        }
