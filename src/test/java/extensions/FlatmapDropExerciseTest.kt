56ee79f7a5a45a1e8f81da529bf9be77393876e4
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-10-13 13:56:52 +0100

    FlatmapDrop exercise

---
+++ src/test/java/extensions/FlatmapDropExerciseTest.kt
@@ -0,0 +1,27 @@
+package extensions
+
+import io.reactivex.schedulers.TestScheduler
+import io.reactivex.subjects.PublishSubject
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+class FlatmapDropExerciseTest {
+
+    @Test
+    fun should_not_re_trigger_network_request_upon_second_click() {
+        val testScheduler = TestScheduler()
+        FlatmapDropExercise<Unit>(testScheduler).run {
+            val clicks = PublishSubject.create<Unit>()
+
+            val testObserver = clicks.multipleClicksDoesNotTriggerNewRequestWithoutFlatmapDrop().test()
+
+            clicks.onNext(Unit)
+            clicks.onNext(Unit)
+            testObserver.assertNoValues()
+
+            testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+
+            testObserver.assertValue(0)
+        }
+    }
+}
