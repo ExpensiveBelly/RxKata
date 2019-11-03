c8ff7211d85c6c45534dbd902fab557c922ec875
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-11-03 20:10:27 +0000

    Add sample tests just to make it fail on purpose before exercise is done

---
+++ src/test/java/playground/basket/BasketItemsExerciseTest.kt
@@ -0,0 +1,11 @@
+package playground.basket
+
+import org.junit.Test
+
+class BasketItemsExerciseTest {
+
+    @Test
+    fun sample_test_to_make_it_fail() {
+        TODO("add tests at your convenience to test the logic")
+    }
+}
---
+++ src/test/java/playground/stream/TweetsPostsExerciseTest.kt
@@ -0,0 +1,11 @@
+package playground.stream
+
+import org.junit.Test
+
+class TweetsPostsExerciseTest {
+
+    @Test
+    fun sample_test_to_make_it_fail() {
+        TODO("add tests at your convenience to test the logic")
+    }
+}
