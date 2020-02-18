21479ba91254943b02785af0289ecce4e706e8dc
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-02-18 20:50:12 +0000

    Maybe To Single exercise

--- src/main/java/playground/maybe/MaybeToSingle.kt
+++ src/main/java/playground/maybe/MaybeToSingle.kt
@@ -1,12 +1,27 @@
 package playground.maybe
 
 import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
 class MaybeToSingle {
 
     val single = Observable.just(true)
-        .doOnSubscribe { println("Subscribed") }
         .filter(true::equals)
         .repeat()
         .singleOrError()
+
+    val subject = PublishSubject.create<Unit>()
+    val subjectStop = PublishSubject.create<Unit>()
+
+
+    val observableFun = { scheduler: Scheduler ->
+        subject
+            .flatMap {
+                Observable.interval(500, TimeUnit.MILLISECONDS, scheduler)
+                    .takeUntil(subjectStop)
+                    .repeat()
+            }
+    }
 }
--- src/test/java/playground/maybe/MaybeToSingleTest.kt
+++ src/test/java/playground/maybe/MaybeToSingleTest.kt
@@ -1,13 +1,34 @@
 package playground.maybe
 
+import io.reactivex.schedulers.TestScheduler
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 class MaybeToSingleTest {
 
     private val maybeToSingle = MaybeToSingle()
 
     @Test
-    fun `should throw IllegalArgumentException sequence contains more than one element because of the repeat`() {
+    fun `should throw IllegalArgumentException because the sequence contains more than one element due to the repeat`() {
         maybeToSingle.single.test().assertError(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `should reset the counter in the interval operator when the subjectStop emits`() {
+        val testScheduler = TestScheduler()
+        val testObserver = maybeToSingle.observableFun(testScheduler).test()
+
+        maybeToSingle.subject.onNext(Unit)
+
+        testScheduler.advanceTimeBy(500, TimeUnit.MILLISECONDS)
+        testObserver.assertValue(0)
+
+        maybeToSingle.subjectStop.onNext(Unit)
+
+        testScheduler.advanceTimeBy(500, TimeUnit.MILLISECONDS)
+        testObserver.assertValues(0, 0) //Re-subscription because of the `repeat`
+
+        testScheduler.advanceTimeBy(500, TimeUnit.MILLISECONDS)
+        testObserver.assertValues(0, 0, 1)
     }
 }
