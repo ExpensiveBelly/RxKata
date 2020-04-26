9f94566b2845a2c600b72ea8569ffdfd0bb69c1b
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-04-26 20:54:01 +0100

    Disposal test

---
+++ src/test/java/playground/disposal/DisposalTest.kt
@@ -0,0 +1,53 @@
+package playground.disposal
+
+import com.nhaarman.mockitokotlin2.inOrder
+import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.verify
+import io.reactivex.rxjava3.core.Observable
+import org.amshove.kluent.mock
+import org.junit.Test
+
+class DisposalTest {
+
+    private interface RxLifecycle {
+        fun doFinally()
+        fun doOnDispose()
+        fun doOnSubscribe()
+    }
+
+    /*
+    Explanation: subscribe disconnects the upstream Disposable when it receives a terminal event thus you can't dispose an already completed sequence
+    https://github.com/ReactiveX/RxJava/issues/5264
+     */
+
+    @Test
+    fun subscribe_dispose() {
+        val verifier = mock<RxLifecycle>()
+
+        Observable.fromCallable { "2" }
+            .doOnSubscribe { verifier.doOnSubscribe() }
+            .doOnDispose { verifier.doOnDispose() }
+            .subscribe()
+            .dispose()
+
+        verify(verifier).doOnSubscribe()
+        verify(verifier, never()).doOnDispose()
+    }
+
+    //What is the fix?
+    @Test
+    fun subscribe_dispose_finally() {
+        val verifier = mock<RxLifecycle>()
+
+        Observable.fromCallable { "2" }
+            .doOnSubscribe { verifier.doOnSubscribe() }
+            .doFinally { verifier.doFinally() }
+            .subscribe()
+            .dispose()
+
+        inOrder(verifier) {
+            verify(verifier).doOnSubscribe()
+            verify(verifier).doFinally()
+        }
+    }
+}
