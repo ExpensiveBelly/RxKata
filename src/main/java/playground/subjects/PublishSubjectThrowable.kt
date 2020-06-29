1ab0e186000c5ec08b9791f4fc127f86505b21c0
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-06-29 15:56:09 +0100

    Add note about Completed vs Disposed

--- src/main/java/playground/subjects/PublishSubjectThrowable.kt
+++ src/main/java/playground/subjects/PublishSubjectThrowable.kt
@@ -8,6 +8,10 @@ import io.reactivex.subjects.PublishSubject
  *
  * The Rx chain errors downstream and disposes upstream. Because of that, the subject has not reached a terminal state (complete, error)
  * so it can be subscribed onto again.
+ *
+ * REMEMBER: A Subject that Completes or Errors becomes unusable and future subscriptions in an attempt to emit values will not work.
+ *
+ * A Subject that is disposed though will still work fine and future subscriptions in an attempt to emit values to the Subject `.onNext(..)` w
  */
 
 class PublishSubjectThrowable {
