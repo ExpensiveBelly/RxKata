5a30158c397498da192301736eb90744c9b4190f
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-07-05 21:14:59 +0100

    Exercise: implement distinctUntilChanged operator using other operators

---
+++ src/main/java/operators/DistinctUntilChangedExtensions.kt
@@ -0,0 +1,9 @@
+package operators
+
+import io.reactivex.Observable
+
+/**
+ * DistinctUntilChanged implementation using other Rx operators
+ */
+
+fun <T : Any> Observable<T>.distinctUntilChanged2(): Observable<T> = TODO("Not implemented")
